2b1cebdc40c877ebc6a226190608d76c14cdcf5e
Andrey Sheshukov <ash@jinr.ru>
2021-01-28 22:03:10 +0300

    Correct usage of RegionsQueue

--- snap/combine/buffer.py
+++ snap/combine/buffer.py
@@ -5,19 +5,22 @@ from .regions import RegionsQueue
 import numpy as np
               
 class Buffer:
-    def __init__(self, delay=1, timeout=0, margin=[0,0]):
+    def __init__(self, delay=1, timeout=6000, margin=[0,0]):
         self.db = DataBuffer()
-        self.regions = RegionsQueue(margin=margin, delay=delay)
+        self.regions = RegionsQueue(delay=delay)
+        self.timeout = timeout
 
     async def put(self, data):
-        t_drop = data.T1()-timeout
+        t_drop = data.T1()-self.timeout
         self.db.put(data)
         await self.regions.put(data.T0(),data.T1())
+        self.db.drop_tail(t_drop)
 
     async def get(self):
         T0,T1 =  await self.regions.get()
         ts = self.db.slice_ts(T0,T1)
-        zs = np.stack(self.db.at(ts)).T
+        tc = 0.5*(ts[:-1]+ts[1:])
+        zs = self.db.at(tc)
         ids = list(self.db.clients.keys())
         return DataBlock(id=ids,ts=ts,zs=zs)
 
