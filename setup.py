0f8736836f7dd4bf965c805094d2694bd5d9155f
Andrey Sheshukov <ash@jinr.ru>
2021-01-29 21:51:55 +0300

    No need for Pandas. Also added requirements

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(name='snap-combine',
-        version='0.1',
+        version='0.5',
         description='SuperNova Async Pipeline: combining',
         author='Andrey Sheshukov',
         author_email='ash@jinr.ru',
         packages=['snap.combine','snap.util','snap.client'],
         py_modules=['snap.datablock'],
-        install_requires=['numpy','pandas',
+        install_requires=['numpy',
                           'snap-base >=1.0.1',
                           'sn_stat',
         ]
