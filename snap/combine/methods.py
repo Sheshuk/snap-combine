605d82fbdadd24c6b0ff9151f6c5f985e1e0318a
Andrey Sheshukov <ash@jinr.ru>
2021-01-29 18:01:51 +0300

    Fixed and tested combination methods

--- snap/combine/methods.py
+++ snap/combine/methods.py
@@ -1,29 +1,32 @@
 import numpy as np
+from scipy.stats import norm,chi2
+from ..datablock import DataBlock
+def z2p(z):
+    return norm.sf(z)
+def p2z(p):
+    return norm.isf(p)
+def x2p(x, Nexp):
+    return chi2.sf(x, df=2*Nexp)
 
-def Fisher(dfi):
-    from scipy.stats import norm,chi2
-    def z2p(z):
-        return norm.sf(z)
-    def p2z(p):
-        return norm.isf(p)
-    def x2p(x, Nexp):
-        return chi2.sf(x, df=2*Nexp)
-    
-    zs = np.nan_to_num(dfi.z.values.T[:,:-1])
-    ps = z2p(zs)
-    X = -2*np.sum(np.log(ps),axis=0)
-    p1 = x2p(X, Nexp=zs.shape[0])
-    z1 = p2z(p1)
-    return z1
+def Fisher(data: DataBlock) -> DataBlock:
+    zs = np.ma.masked_invalid(data.zs)
+    ps = np.ma.masked_array(z2p(zs),zs.mask)
+    X = -2*np.sum(np.log(ps),axis=1)
+    assert X.shape==(zs.shape[0],)
+    Nexp = (zs.mask==False).sum(axis=1)
+    pc = x2p(X, Nexp=Nexp)
+    zc = p2z(pc)
+    return DataBlock(ts=data.ts,zs=zc)
 
-class Stouffer:
-    def __init__(self, weights):
-        self.weights=weights
-    def __call__(self,dfi):
-        zs = np.nan_to_num(dfi.z.values.T[:,:-1])
-        ws = np.array([self.weights.get(e,0) for e in dfi.z.columns])
-        w2 = np.sqrt(np.sum(ws**2))
-        zs = (ws*zs.T).sum(axis=1)
-        return zs/w2 if w2>0 else 0
-
-
+def Stouffer(weights: dict):
+    def _f(data: DataBlock) -> DataBlock:
+        zs = np.ma.masked_invalid(data.zs)
+        #calculate weights for each point
+        ws = np.array([weights[c] for c in data.id])
+        ws = ws*(zs.mask==False)
+        #calculate weights norm
+        w2 = np.sqrt(np.sum(ws**2, axis=1))
+        zc = (ws*zs).sum(axis=1)
+        zc = (zc/w2).T
+        return DataBlock(ts=data.ts,zs=zc)
+    return _f
