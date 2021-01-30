9f11d6bd60815a4b0db1f4725251a48d6d18d0cc
Andrey Sheshukov <dxdydz2@gmail.com>
2021-01-30 18:48:36 +0300

    v0.5.3

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@
 
 from setuptools import setup
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(name='snap-combine',
-        version='0.5.2',
+        version='0.5.3',
         description='SuperNova Async Pipeline: combining',
+        long_description=readme,
+        long_description_content_type='text/markdown',
         author='Andrey Sheshukov',
         author_email='ash@jinr.ru',
         packages=['snap.combine','snap.util','snap.client'],
