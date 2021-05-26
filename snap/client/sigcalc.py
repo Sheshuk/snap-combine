770b5e4d7b9c2cd43df04e5162cb71245dc252d5
Andrey Sheshukov <ash@jinr.ru>
2021-05-26 17:29:00 +0300

    Common interface for the shape and counting analysis

--- snap/client/sigcalc.py
+++ snap/client/sigcalc.py
@@ -1,12 +1,12 @@
-from sn_stat import SigCalc, LLR
+import sn_stat as sn
 from snap.datablock import DataBlock
 import numpy as np
 from snap.util import timing
 
-class ShapeAnalysis:
+class SignificanceCalculator:
     """Significance calculator using shape analysis
     
-       Calculates SN observation significance using shape analysis method, 
+       Calculates SN observation significance using the analysis method, 
        based on the *sn_stat* module.
 
        Calculator consumes the data timestamps (via :ShapeAnalysis.put: method) 
@@ -14,21 +14,19 @@ class ShapeAnalysis:
 
 
        """
-    def __init__(self,S,B, time_window, dt=0.1, tChunk_min=1, **params):
+    def __init__(self, ana, dt=0.1, tChunk_min=1):
         """
         Create significance calculator with given bg and sg rates
 
-        params:
-        -------
-            * S, B: signal and background rates (see note)
-            * time_window: tuple (t0,t1) - a range around expected supernova time,
-            where we perform analysis
-            * dt: float, time step of the start location
-            * tChunk_min: minimal time duration of the produced chunk of data
+        Args:
+            ana (:class:`sn.ShapeAnalysis` or :class:`sn.CountingAnalysis` object):
+                analysis to be used for the significance calculation
+            dt (float): 
+                time step, seconds
+            tChunk_min (float): 
+                minimal time duration of the produced chunk of data
         """
-        llr = LLR(S,B,time_window)
-        self.sc = SigCalc([llr],**params)
-        self.time_window = time_window
+        self.ana = ana
         self.t0 = timing.now()
         self.dt=dt
         self.tChunk_min = tChunk_min
@@ -44,15 +42,27 @@ class ShapeAnalysis:
 
     async def get(self) -> DataBlock:
         """Calculate the significance and return it in a :snap.DataBlock: """
-        tw0,tw1 = self.time_window 
+        tw0,tw1 = self.ana.det.time_window 
         time_start = self.t0+tw1-tw0
         await timing.wait_until(time_start, self.tChunk_min)
         t1 = timing.now()
         #define time regions
         ts = np.arange(self.t0-tw0,t1-tw1, self.dt)
         #calculate significance
-        zs = self.sc([self.data], ts)
+        zs = self.ana(self.data, ts)
         #drop obsolete data
         t_last = ts[-1]+self.dt
         self.drop_tail(t_last + tw0)
         return DataBlock(np.append(ts,t_last),zs)
+
+def CountAna(B, time_window, *, dt=0.1, tChunk_min=1):
+    ana = sn.CountingAnalysis(
+            sn.DetConfig(B=B, time_window=time_window)
+            )
+    return SignificanceCalculator(ana, dt, tChunk_min)
+
+def ShapeAna(B,S, time_window="auto", *, dt=0.1, tChunk_min=1):
+    ana = sn.ShapeAnalysis(
+            sn.DetConfig(B=B,S=S,time_window=time_window)
+            )
+    return SignificanceCalculator(ana, dt, tChunk_min)
