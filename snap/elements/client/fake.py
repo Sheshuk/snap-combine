23fa876f010cfd0bf4ec709d79ea6412acfbb264
Andrey Sheshukov <ash@jinr.ru>
2021-09-01 14:46:18 +0300

    Add delay for data generation and sigcalc

--- snap/elements/client/fake.py
+++ snap/elements/client/fake.py
@@ -1,10 +1,7 @@
 import asyncio
 import sn_stat as st
 import numpy as np
-from datetime import datetime
-
-def now():
-    return datetime.now().timestamp()
+from snap import timing
 
 async def sample_ts(B, S=0, tSN=0, tChunk=10):
     """ Data :term:`source`, generating random event timestamps 
@@ -20,24 +17,31 @@ async def sample_ts(B, S=0, tSN=0, tChunk=10):
     Yields:
         event timestamp in seconds
     """
-    t0 = now()
+    t0 = timing.now()
     R = st.rate(B)+st.rate(S).shift(t0+tSN)
     while True:
         ts = st.Sampler(R,time_window=[t0,t0+tChunk]).sample()
         yield np.sort(ts)
         t0+=tChunk
 
-async def realtime(source):
+def realtime(shift:float=0):
     """ 
     Output each timestamp from the list at their appropriate time
 
     :Input: list of unix timestamps
     :Output: yield each timestamp individually at their time.
+
+    Args:
+        shift
+            time shift in seconds
     """
-    async for data in source:
-        for t in np.sort(data):
-            await asyncio.sleep(t-now())
-            yield t
+
+    async def _f(source):
+        async for data in source:
+            for t in np.sort(data):
+                await timing.wait_until(t+shift)
+                yield t
+    return _f
 
 class Generator:
     def __init__(self, B, S=0, tChunk=10):
@@ -45,13 +49,13 @@ class Generator:
         self.tChunk=tChunk
         self.B=st.rate(B)
         self.R = self.B
-        self.t0 = now()
+        self.t0 = timing.now()
 
     async def put(self, command):
         tSN = command['tSN']
         dist =command['dist']
         self.R = self.B+self.S.at(dist).shift(tSN)
-        print(f"Expecting supernova @{dist}kpc at t={tSN} (in {tSN-now()} seconds!")
+        print(f"Expecting supernova @{dist}kpc at t={tSN} (in {tSN-timing.now()} seconds!")
 
     async def get(self):
         ts = st.Sampler(self.R,
@@ -61,6 +65,6 @@ class Generator:
 
 async def detonator(delay=10):
     while True:
-        tSN = now()+delay
+        tSN = timing.now()+delay
         dist = 1.
         yield {'tSN':tSN, 'dist':dist}
