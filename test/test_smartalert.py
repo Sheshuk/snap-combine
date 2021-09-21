a3c00040286e4a65ea0d0779b6bdee89ede93b38
Andrey Sheshukov <dxdydz2@gmail.com>
2021-09-21 15:31:01 -0500

    Change alert orders by Zmax. Add test for similar data

--- test/test_smartalert.py
+++ test/test_smartalert.py
@@ -54,6 +54,18 @@ async def test_cluster_rewritten_by_second_data_gives_DEL():
     with pytest.raises(asyncio.TimeoutError):
         await get(sa)
 
+@pytest.mark.asyncio
+async def test_cluster_repeating_same_data_gives_nothing():
+    sa = SmartAlert(threshold=0, timeout=600)
+    d0 = DataBlock(ts=np.arange(11),zs=[0,0,0,1,1,1,1,1,0,0])
+    await sa.put(d0)
+    assert await get(sa) == ("NEW",DataBlock(id=1,ts=range(3,9),zs=[1,1,1,1,1]))
+    with pytest.raises(asyncio.TimeoutError):
+        await get(sa)
+    await sa.put(d0)
+    with pytest.raises(asyncio.TimeoutError):
+        await get(sa)
+
 async def dump_all(obj):
     print('--beg')
     while True:
@@ -69,8 +81,8 @@ async def test_two_clusters_in_one_block():
     sa = SmartAlert(threshold=0, timeout=600)
     d0 = DataBlock(ts=np.arange(0, 11),zs=[0,1,1,1,0,0,2,2,2,0])
     await sa.put(d0)
-    assert await get(sa) == ("NEW",DataBlock(id=1,ts=[1,2,3,4], zs=[1,1,1]))
-    assert await get(sa) == ("NEW",DataBlock(id=2,ts=[6,7,8,9], zs=[2,2,2]))
+    assert await get(sa) == ("NEW",DataBlock(id=1,ts=[6,7,8,9], zs=[2,2,2]))
+    assert await get(sa) == ("NEW",DataBlock(id=2,ts=[1,2,3,4], zs=[1,1,1]))
     
     with pytest.raises(asyncio.TimeoutError):
         await get(sa)
@@ -82,11 +94,11 @@ async def test_two_clusters_merge_by_z_order():
     d1 = DataBlock(ts=np.arange(0, 11),zs=[0,0,0,2,2,2,2,0,1,1])
     await sa.put(d0)
     await sa.put(d1)
-    assert await get(sa) == ("NEW",DataBlock(id=1,ts=[1,2,3,4], zs=[1,1,1]))
-    assert await get(sa) == ("NEW",DataBlock(id=2,ts=[6,7,8,9], zs=[2,2,2]))
-    assert await get(sa) == ("UPD",DataBlock(id=2,ts=[3,4,5,6,7], zs=[2,2,2,2]))
-    assert await get(sa) == ("DEL",DataBlock(id=1,ts=[1,2,3,4], zs=[1,1,1]))
-    assert await get(sa) == ("NEW",DataBlock(id=1,ts=[8,9,10], zs=[1,1]))
+    assert await get(sa) == ("NEW",DataBlock(id=1,ts=[6,7,8,9], zs=[2,2,2]))
+    assert await get(sa) == ("NEW",DataBlock(id=2,ts=[1,2,3,4], zs=[1,1,1]))
+    assert await get(sa) == ("UPD",DataBlock(id=1,ts=[3,4,5,6,7], zs=[2,2,2,2]))
+    assert await get(sa) == ("DEL",DataBlock(id=2,ts=[1,2,3,4], zs=[1,1,1]))
+    assert await get(sa) == ("NEW",DataBlock(id=3,ts=[8,9,10], zs=[1,1]))
     
     with pytest.raises(asyncio.TimeoutError):
         await get(sa)
@@ -98,10 +110,10 @@ async def test_two_clusters_merge_to_two():
     d1 = DataBlock(ts=np.arange(0, 11),zs=[0,0,0,1,1,1,1,0,2,2])
     await sa.put(d0)
     await sa.put(d1)
-    assert await get(sa) == ("NEW",DataBlock(id=1,ts=[1,2,3,4], zs=[1,1,1]))
-    assert await get(sa) == ("NEW",DataBlock(id=2,ts=[6,7,8,9], zs=[2,2,2]))
-    assert await get(sa) == ("UPD",DataBlock(id=2,ts=[8,9,10], zs=[1,1]))
-    assert await get(sa) == ("UPD",DataBlock(id=1,ts=[3,4,5,6,7], zs=[1,1,1,1]))
+    assert await get(sa) == ("NEW",DataBlock(id=1,ts=[6,7,8,9], zs=[2,2,2]))
+    assert await get(sa) == ("NEW",DataBlock(id=2,ts=[1,2,3,4], zs=[1,1,1]))
+    assert await get(sa) == ("UPD",DataBlock(id=1,ts=[8,9,10], zs=[2,2]))
+    assert await get(sa) == ("UPD",DataBlock(id=2,ts=[3,4,5,6,7], zs=[1,1,1,1]))
     
     with pytest.raises(asyncio.TimeoutError):
         await get(sa)
